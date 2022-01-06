733f32e4f86cffeb9362e80e3bda61838c9b00cf
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-01-06 18:16:21 -0500

    Add Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./src/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['Popins', 'sans-serif']
+    },
+    extend: {}
+  },
+  plugins: []
+}
