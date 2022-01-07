30d13614bf08da31d8e091d131385ad2c1e28f4f
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-01-07 01:06:58 -0500

    style: fix sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom'
 
 function Sidebar({ links, buttons }) {
   return (
-    <aside className="flex flex-col flex-auto sticky top-0 overflow-hidden h-screen flex-shrink-0 z-20 justify-between shadow-5 bg-white text-center px-5 py-10 max-w-[280px]">
-      <div className="h-32 grid place-content-center text-2xl font-bold">
+    <aside className="flex flex-col flex-auto sticky top-0 overflow-hidden h-screen flex-shrink-0 z-20 justify-between shadow-5 bg-white text-center p-4 max-w-[280px]">
+      <div className="h-20 grid place-content-center text-2xl font-bold">
         EduTECH
       </div>
       <nav>
@@ -15,7 +15,7 @@ function Sidebar({ links, buttons }) {
           ))}
         </ul>
       </nav>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col justify-center gap-4 h-40">
         {buttons.map((button, index) => (
           <button className="btn" key={index}>
             {button}
