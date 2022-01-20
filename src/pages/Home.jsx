0b853f01b88c72426f90045003674907cf26733b
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-01-19 21:41:16 -0500

    feat: first draft for Login page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ function Home() {
   return (
     <div className="grid h-screen place-content-center text-center">
       <h1>EduTECH Homepage</h1>
-      <Link to="/dashboard" className="btn mt-2">
-        Dashboard
+      <Link to="/login" className="btn mt-2">
+        Login
       </Link>
     </div>
   )
