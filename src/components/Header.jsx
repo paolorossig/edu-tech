3f2e3c6ff48e164b78d98c442d53161cb4596a16
Paolo Rossi <paolo.rossi@pucp.edu.pe>
2022-01-25 17:27:54 -0500

    style: fix z-index and cart icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom'
+import { BsCartPlusFill } from 'react-icons/bs'
+
 function Header() {
   return (
     <header className="sticky top-0 z-20 bg-gray-100 py-4 px-8">
       <div className="flex h-20 items-center justify-end rounded-xl bg-white p-4 shadow-md">
+        <Link
+          to="checkout"
+          className="mr-4 text-2xl text-gray-500 hover:text-black"
+        >
+          <BsCartPlusFill />
+        </Link>
         <div className="flex cursor-pointer gap-2">
           <div className="h-10 w-10 rounded-full bg-black"></div>
           <div className="flex flex-col">
