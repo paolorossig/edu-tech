bebbb342627519b1b3995eba43c0de86a97cfc47
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-02-27 13:32:44 -0500

    feat: add Dropdown Menu in Dashboard Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,26 @@
 import { Link } from 'react-router-dom'
 import { BsCartPlusFill } from 'react-icons/bs'
+import Dropdown from './Dropdown'
 
 function Header() {
   return (
     <header className="sticky top-0 z-20 bg-gray-100 py-4 px-8">
-      <div className="flex h-20 items-center justify-end rounded-xl bg-white p-4 shadow-md">
+      <div className="flex items-center justify-end rounded-xl bg-white p-2 shadow-md">
         <Link
           to="checkout"
           className="mr-4 text-2xl text-gray-500 hover:text-black"
         >
           <BsCartPlusFill />
         </Link>
-        <div className="flex cursor-pointer gap-2">
-          <div className="h-10 w-10 rounded-full bg-black"></div>
-          <div className="flex flex-col">
-            <h3>Paolo Rossi</h3>
-            <h4>Alumno</h4>
+        <Dropdown>
+          <div className="flex gap-2">
+            <div className="h-10 w-10 rounded-full bg-black"></div>
+            <div className="flex flex-col items-start">
+              <h3>Paolo Rossi</h3>
+              <h4>Alumno</h4>
+            </div>
           </div>
-        </div>
+        </Dropdown>
       </div>
     </header>
   )
