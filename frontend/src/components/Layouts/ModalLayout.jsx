40ac6bb0d3c6daf1c9a25321696c8d73e55ebbbe
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-03-13 11:28:08 -0500

    style: add responsive to Login

--- frontend/src/components/Layouts/ModalLayout.jsx
+++ frontend/src/components/Layouts/ModalLayout.jsx
@@ -1,12 +1,12 @@
 function ModalLayout({ imgsrc, children }) {
   return (
-    <div className="grid h-screen place-content-center text-center">
-      <div className="flex overflow-hidden rounded-3xl shadow-md">
-        <div className="flex min-w-[300px] flex-col justify-center bg-white p-8">
+    <div className="grid h-screen w-screen place-content-center text-center">
+      <div className="flex h-screen w-screen flex-col-reverse justify-center overflow-hidden bg-white md:h-full md:w-full md:flex-row md:rounded-3xl md:shadow-md">
+        <div className="flex min-w-[350px] flex-col justify-center px-8 py-4 md:py-8">
           {children}
         </div>
-        <div className="grid max-w-md place-content-center bg-slate-200 p-8">
-          <img src={imgsrc} alt="auth-image" />
+        <div className="flex h-full w-full max-w-md justify-center self-center px-8 py-4 md:bg-slate-200 md:py-8">
+          <img src={imgsrc} alt="auth-image" className="" />
         </div>
       </div>
     </div>
