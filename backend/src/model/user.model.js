8e54d43ba4ce23f7ff2c4b8f7f01b84fc245a6ff
FernandoCalla <fernandorafaelcallayarihuaman@gmail.com>
2022-03-14 16:19:04 -0500

    feat:add new details to userSchema

--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -4,7 +4,20 @@ import { hashSync, compare } from 'bcrypt'
 const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    firstName: { type: String, default: '' },
+    lastName: { type: String, default: '' },
+    surName: { type: String, default: '' },
+    role: { type: String, default: '' },
+    nickName: { type: String, default: '' },
+    // photoURL: { type: String, required: true, default: '' },
+    sexo: { type: String, default: '' },
+    dni: { type: String, default: '' },
+    birthday: { type: String, default: '' },
+    occupation: { type: String, default: '' },
+    phoneNumber: { type: String, default: '' },
+    country: { type: String, default: '' }
+    // authProvider: { type: String, required: true, default: '' }
   },
   { timestamps: true }
 )
