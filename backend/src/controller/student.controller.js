c4b42fa312a84105c27c66bc77af094a21e183a2
FernandoCalla <fernandorafaelcallayarihuaman@gmail.com>
2022-03-22 15:42:04 -0500

    feat:add function to send mail

--- backend/src/controller/student.controller.js
+++ backend/src/controller/student.controller.js
@@ -1,8 +1,10 @@
 import { createStudent, updateStudent } from '../service/student.service.js'
+import { sendMail } from '../utils/mailer.js'
 
 export async function createStudentHandler(req, res) {
   try {
     const student = await createStudent(req.body)
+    await sendMail(student.user)
     res.status(201).json({ message: 'Student created', student })
   } catch (error) {
     res.status(400).json({ message: error.message })
