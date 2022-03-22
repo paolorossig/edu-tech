5ca95f0f6179455c3081012e0d54046dbc313722
FernandoCalla <fernandorafaelcallayarihuaman@gmail.com>
2022-03-22 15:39:41 -0500

    feat:add cloudinary config

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,8 @@
+import cloudinary from 'cloudinary'
+export function connectCloudinary() {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+  })
+}
