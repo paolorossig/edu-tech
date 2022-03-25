062239f025e4d5f175d5b9b13105469175885a81
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-03-25 13:33:36 -0500

    fix: lock, log and card

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,8 +1,11 @@
 import cloudinary from 'cloudinary'
+import log from './logger.js'
+
 export function connectCloudinary() {
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
   })
+  log.info('Cloudinary configured')
 }
