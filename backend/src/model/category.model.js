4441f635db861a1f63960d1e287c6e5a8c95d5d0
FernandoCalla <fernandorafaelcallayarihuaman@gmail.com>
2022-03-26 13:25:48 -0500

    refactor the name of attribute on category model

--- backend/src/model/category.model.js
+++ backend/src/model/category.model.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const categorySchema = new mongoose.Schema(
   {
-    description: { type: String, required: true, unique: true } // authProvider: { type: String, required: true, default: '' }
+    name: { type: String, required: true, unique: true } // authProvider: { type: String, required: true, default: '' }
   },
   { timestamps: true }
 )
