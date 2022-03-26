6368388fa4b382539da7e5fb15512531c90e6bcb
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-03-25 23:47:47 -0500

    refactor: welcome mail

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -1,31 +1,25 @@
 import sgMail from '@sendgrid/mail'
+import log from './logger.js'
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-const welcomeMail = async (message) => {
+export const sendMail = async (message) => {
   try {
     await sgMail.send(message)
-    console.log('Mensaje enviado')
+    log.info('Mail sent')
   } catch (error) {
-    console.log('Error: ', error)
+    log.child({ error }).error('sendgrid error')
   }
 }
 
-export function sendMailConfigUser(user) {
-  return welcomeMail({
+export function sendWelcomeMail(user) {
+  return sendMail({
     to: user.email,
     from: 'eduteach.makeitreal@gmail.com',
-    subject: 'Bienvenid@ a eduTech',
-    text: `Hola, terminaste de configurar tu cuenta como ${user.role}`,
-    html: `<h1>Cuenta configurada</h1><h2>Hola, terminaste de configurar tu cuenta como ${user.role}</h2>`
-  })
-}
-export function sendMailCreateUser(user) {
-  return welcomeMail({
-    to: user.email,
-    from: 'eduteach.makeitreal@gmail.com',
-    subject: 'Bienvenid@ a eduTech',
-    text: `Hola, tu usuario ya fue creado, bienvenido a eduTech.`,
-    html: `<h1>Welcome to EduTech</h1>`
+    subject: 'Bienvenid@ a EduTECH',
+    html: `<h1>Hola ${user.firstName},</h1>
+    <p>Acabas de terminar de configurar tu cuenta como ${user.role}!</p>
+    <p>Para poder acceder a la plataforma, ingresa a la siguiente dirección:</p>
+    <p>${process.env.CLIENT_URL}/login</p>`
   })
 }
