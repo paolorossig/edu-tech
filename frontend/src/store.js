5aa40bedd4b3e63bbd778a144d4b2a55de892bca
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-04-01 00:58:28 -0500

    feat: add courseApi

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import categoriesReducer from '@/features/categories/categorySlice'
+import { courseApi } from './features/courses/CourseSlice'
 
 const store = configureStore({
   reducer: {
-    categories: categoriesReducer
-  }
+    categories: categoriesReducer,
+    [courseApi.reducerPath]: courseApi.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(courseApi.middleware)
 })
 
 export default store
