f59870aa3a43c788dca33fbe9a23651f90dab564
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-04-01 00:04:08 -0500

    style: table classes and responsiveness

--- frontend/src/components/Teacher/CoursesList.jsx
+++ frontend/src/components/Teacher/CoursesList.jsx
@@ -17,50 +17,40 @@ function CoursesList() {
   }, [])
 
   return (
-    <div>
-      <h1>Cursos creados</h1>
-      <table className="mb-4 min-w-full divide-y divide-gray-200">
-        <thead className="bg-gray-50">
+    <section>
+      <h1 className="mb-4">Cursos creados</h1>
+      <table className="mb-6 table">
+        <thead>
           <tr>
-            <td className="whitespace-nowrap px-6 py-4">
-              <strong>#</strong>
-            </td>
-            <td className="whitespace-nowrap px-6 py-4">
-              <strong>Curso</strong>
-            </td>
-            <td className="whitespace-nowrap px-6 py-4">
-              <strong>Categoria</strong>
-            </td>
-            <td className="whitespace-nowrap px-6 py-4">
-              <strong>Precio</strong>
-            </td>
-            <td className="whitespace-nowrap px-6 py-4">
-              <strong>URL portada</strong>
-            </td>
+            <th className="table_head">#</th>
+            <th className="table_head text-left">Curso</th>
+            <th className="table_head">Categoría</th>
+            <th className="table_head">Precio</th>
+            <th className="table_head">URL portada</th>
           </tr>
         </thead>
-        <tbody className="divide-y divide-gray-200 bg-white">
+        <tbody className="table_body">
           {courses.map((item, index) => (
             <tr key={item._id}>
-              <td className="whitespace-nowrap px-6 py-4">{index + 1}</td>
-              <td className="whitespace-nowrap px-6 py-4">{item.name}</td>
-              <td className="whitespace-nowrap px-6 py-4">{item.category}</td>
-              <td className="whitespace-nowrap px-6 py-4">{item.price}</td>
-              <td className="whitespace-nowrap px-6 py-4">
+              <td className="table_row">{index + 1}</td>
+              <td className="table_row text-left">{item.name}</td>
+              <td className="table_row">{item.category.name}</td>
+              <td className="table_row">{item.price}</td>
+              <td className="table_row flex justify-center">
                 <img
                   src={item.imageURL}
                   alt="course-image"
-                  className="w-26 h-10 object-cover"
+                  className="h-10 w-16 object-cover"
                 />
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-      <Button type="submit" onClick={() => navigate('createCourse')}>
+      <Button type="submit" onClick={() => navigate('create')}>
         Crear curso
       </Button>
-    </div>
+    </section>
   )
 }
 export default CoursesList
--- frontend/src/pages/teacher/TeacherHome.jsx
+++ frontend/src/pages/teacher/TeacherHome.jsx
@@ -1,52 +1,22 @@
 import Card from '@/components/Card'
 import TableTeacher from '@/components/Teacher/TableTeacher'
-import { cursos, estadisticas } from '@/data/cursos.json'
+import { cursos } from '@/data/cursos.json'
+import { Link } from 'react-router-dom'
 
 function TeacherHome() {
   return (
-    <div>
-      <h1 className="mb-4">Tus cursos</h1>
-      <div className="mb-4 flex gap-8">
-        {cursos.map((curso, index) => (
-          <Card element={curso} key={index} />
-        ))}
-      </div>
-      <h1>Estadísticas de tus cursos</h1>
-      <table className="min-w-full divide-y divide-gray-200">
-        <thead className="bg-gray-50">
-          <tr>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
-            >
-              Cursos
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
-            >
-              Matriculados
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
-            >
-              Conectados
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
-            >
-              Hoy
-            </th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-200 bg-white">
-          {estadisticas.map((curso, index) => (
-            <TableTeacher element={curso} key={index} />
+    <div className="flex flex-col gap-12">
+      <section className="-mr-8">
+        <h1 className="mb-8">Mis cursos</h1>
+        <div className="scrollbar-hide flex gap-8 overflow-y-hidden overflow-x-scroll">
+          {cursos.map((curso) => (
+            <Link to={`courses/${encodeURIComponent(curso.id)}`} key={curso.id}>
+              <Card element={curso} />
+            </Link>
           ))}
-        </tbody>
-      </table>
+        </div>
+      </section>
+      <TableTeacher />
     </div>
   )
 }
