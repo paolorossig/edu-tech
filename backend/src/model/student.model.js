34fa021fa0fb909459f470ccb7481ee5ac64db2e
FernandoCalla <fernandorafaelcallayarihuaman@gmail.com>
2022-04-04 13:17:45 -0500

    refactor teacher and student models

--- backend/src/model/student.model.js
+++ backend/src/model/student.model.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const studentSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    membership: { type: Number, default: 0 },
+    membership: { type: Boolean, default: false },
     interestCategories: [
       { type: mongoose.Schema.Types.ObjectId, ref: 'Category' }
     ],
--- backend/src/model/teacher.model.js
+++ backend/src/model/teacher.model.js
@@ -5,8 +5,7 @@ const teacherSchema = new mongoose.Schema(
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     banckAccount: { type: String, required: true },
     salary: { type: Number },
-    disponibility: [{ day: String, horaInicio: String, horaFinal: String }],
-    createdCursos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
+    disponibility: [{ day: String, horaInicio: String, horaFinal: String }]
   },
   { timestamps: true }
 )
