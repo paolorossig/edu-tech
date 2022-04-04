0a768d415539187b3df9e549bd8a1e1f63c5108f
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-04-04 11:59:32 -0500

    feat: add Basket Counter in students Header

--- frontend/src/features/basket/BasketSlice.js
+++ frontend/src/features/basket/BasketSlice.js
@@ -1,30 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = {
-  selected: [],
-  status: 'idle',
-  error: null
-}
-
 export const BasketSlice = createSlice({
   name: 'basket',
-  initialState,
+  initialState: [],
   reducers: {
     addToBasket: (state, action) => {
-      state.selected.push(action.payload)
+      state.push(action.payload)
     },
     removeItem: (state, action) => {
-      state.selected = state.selected.filter(
-        (item) => item._id !== action.payload
-      )
+      return state.filter((item) => item._id !== action.payload)
     }
   }
 })
 
 export const { addToBasket, removeItem } = BasketSlice.actions
 
-export const selectBasketItems = (state) => state.basket.selected
+export const selectBasketItems = (state) => state.basket
+export const selectTotalItems = (state) => state.basket.length
 export const selectTotalPrice = (state) =>
-  state.basket.selected.reduce((total, item) => total + item.price, 0)
+  state.basket.reduce((total, item) => total + item.price, 0)
 
 export default BasketSlice.reducer
