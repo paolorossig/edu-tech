dece345b5884561f63fb257da8846c598a1bbf3e
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-04-06 18:22:27 -0500

    feat: add React Suspense and lazy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,75 +1,33 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Landing from './pages/Landing'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import SelectUserType from './pages/SelectUserType'
-import Inicio from './pages/dashboard/Inicio'
-import Courses from './pages/dashboard/courses'
-import Course from './pages/dashboard/courses/Course'
-import Lesson from './pages/dashboard/courses/Lesson'
-import Catalogue from './pages/dashboard/catalogue'
-import Sessions from './pages/dashboard/Sessions'
-import Checkout from './pages/dashboard/Checkout'
-import Teachers from './pages/dashboard/teachers'
-import Teacher from './pages/dashboard/teachers/Teacher'
-import TeacherCoursesList from './pages/dashboard/teachers/TeacherCoursesList'
-import Chat from './pages/dashboard/teachers/Chat'
-import TeacherHome from './pages/teacher/TeacherHome'
-import TeacherCourses from './pages/teacher/courses'
-import CreateCourse from './pages/teacher/courses/CreateCourse'
-import NotFound from './pages/NotFound'
+import { lazy, Suspense } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import RequireAuth from './components/RequireAuth'
-import DashboardLayout from './components/Layouts/DashboardLayout'
-import { alumnosNavConfig } from './pages/dashboard/layoutConfig'
-import { teacherNavConfig } from './pages/teacher/teacherConfig'
+import Loading from './components/Loading'
+const Landing = lazy(() => import('./pages/Landing'))
+const Login = lazy(() => import('./pages/Login'))
+const Signup = lazy(() => import('./pages/Signup'))
+const SelectUserType = lazy(() => import('./pages/SelectUserType'))
+const Dashboard = lazy(() => import('./pages/dashboard'))
+const TeacherDashboard = lazy(() => import('./pages/teacher'))
+const NotFound = lazy(() => import('./pages/NotFound'))
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/" element={<Landing />} />
 
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/select-user-type" element={<SelectUserType />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/select-user-type" element={<SelectUserType />} />
 
-      <Route element={<RequireAuth />}>
-        <Route
-          path="/dashboard"
-          element={<DashboardLayout navConfig={alumnosNavConfig} />}
-        >
-          <Route index element={<Inicio />} />
-          <Route path="courses">
-            <Route index element={<Courses />} />
-            <Route path=":courseId" element={<Course />} />
-            <Route path=":courseId/:lessonId" element={<Lesson />} />
-          </Route>
-          <Route path="teachers">
-            <Route index element={<Teachers />} />
-            <Route path=":teacherId" element={<Teacher />}>
-              <Route index element={<Navigate to="courses" />} />
-              <Route path="courses" element={<TeacherCoursesList />} />
-              <Route path="chat" element={<Chat />} />
-            </Route>
-          </Route>
-          <Route path="catalogue" element={<Catalogue />} />
-          <Route path="sesiones" element={<Sessions />} />
-          <Route path="checkout" element={<Checkout />} />
+        <Route element={<RequireAuth />}>
+          <Route path="/dashboard/*" element={<Dashboard />} />
+          <Route path="/teacher/*" element={<TeacherDashboard />} />
         </Route>
 
-        <Route
-          path="/teacher"
-          element={<DashboardLayout navConfig={teacherNavConfig} />}
-        >
-          <Route index element={<TeacherHome />} />
-          <Route path="courses">
-            <Route index element={<TeacherCourses />} />
-            <Route path="create" element={<CreateCourse />} />
-          </Route>
-        </Route>
-      </Route>
-
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
   )
 }
 
