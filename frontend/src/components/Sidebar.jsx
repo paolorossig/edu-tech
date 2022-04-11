828ac74b752afc374303f8d20659feb9fd2122d5
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-04-11 18:59:39 -0500

    style: see Sidebar in mobile view

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,29 +1,55 @@
 import { Link } from 'react-router-dom'
+import { XIcon, MenuAlt2Icon } from '@heroicons/react/outline'
+import useToggle from '@/hooks/useToggle'
 import logo from '@/assets/svg/logo.svg'
 
 function Sidebar({ links, buttons }) {
+  const [open, toggle] = useToggle()
+
   return (
-    <aside className="fixed top-0 z-30 hidden h-screen w-[280px] flex-shrink-0 flex-col items-center justify-between bg-white p-4 text-center shadow-lg md:flex">
-      <Link to="/" className="h-[76px] py-2">
-        <img src={logo} alt="logo" className="h-full cursor-pointer" />
-      </Link>
-      <nav>
-        <ul className="flex flex-col">
-          {links.map((link, index) => (
-            <li className="link" key={index}>
-              <Link to={link.path}>{link.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <div className="flex h-40 w-full flex-col justify-center gap-4 px-10">
-        {buttons?.map((button) => (
-          <Link to={button.path} className="btn" key={button.path}>
-            {button.name}
-          </Link>
-        ))}
+    <>
+      <div className={`${open ? 'hidden' : 'block'} fixed top-0 z-30 w-screen`}>
+        <MenuAlt2Icon
+          onClick={toggle}
+          className="absolute left-6 top-[30px] h-12 w-12 rounded-full bg-gray-100 p-2 text-gray-500"
+        />
       </div>
-    </aside>
+      <aside
+        className={`${
+          open ? 'flex' : 'hidden'
+        } fixed top-0 z-30 h-screen w-screen flex-col items-center justify-between bg-white p-4 text-center shadow-lg md:flex md:w-[280px]`}
+      >
+        <div className="relative h-[76px] w-full py-2">
+          <Link to="/">
+            <img
+              src={logo}
+              alt="logo"
+              className="mx-auto h-full cursor-pointer"
+            />
+          </Link>
+          <XIcon
+            onClick={toggle}
+            className="absolute right-0 top-[18px] h-10 w-10 md:hidden"
+          />
+        </div>
+        <nav>
+          <ul className="flex flex-col">
+            {links.map((link, index) => (
+              <li className="link" key={index}>
+                <Link to={link.path}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className="flex h-40 w-full flex-col justify-center gap-4 px-10">
+          {buttons?.map((button) => (
+            <Link to={button.path} className="btn" key={button.path}>
+              {button.name}
+            </Link>
+          ))}
+        </div>
+      </aside>
+    </>
   )
 }
 
